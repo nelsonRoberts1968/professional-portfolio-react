e02e660292852708fa8c0a90c66898f95505b55a
Nelson Roberts <nellyroberts1968@gmail.com>
2022-03-24 01:58:23 -0700

    temporarily commenting url

--- server/server.js
+++ server/server.js
@@ -11,11 +11,16 @@ const app = express();
 app.use(express.json);
 app.use(cors())
 
-if(process.env.NODE_ENV === "production")
-app.use(express.static("../client/build"))
-app.get("*",(req,res)=>(
-    res.sendFile(path.resolve(__dirname,"../client/build/index.html"))
-))
+// if(process.env.NODE_ENV === "production")
+// app.use(express.static("../client/build"))
+// app.get("*",(req,res)=>(
+//     res.sendFile(path.resolve(__dirname,"../client/build/index.html"))
+// ))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 
 app.listen(port, console.log(`server listenrning to port 3001`))
