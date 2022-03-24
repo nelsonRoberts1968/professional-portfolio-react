71074811570908d5aeaba95e8c3dec1fa8751d00
Nelson Roberts <nellyroberts1968@gmail.com>
2022-03-24 00:49:10 -0700

    updating client build url path

--- server/server.js
+++ server/server.js
@@ -9,9 +9,9 @@ app.use(express.json);
 app.use(cors())
 
 if(process.env.NODE_ENV === "production")
-app.use(express.static("client/build"))
+app.use(express.static("../client/build"))
 app.get("*",(req,res)=>(
-    res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+    res.sendFile(path.resolve(__dirname,"../client","build","index.html"))
 ))
 
 const port = process.env.PORT || 3001;
