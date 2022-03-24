ce5a732dc24924781b39b28aeb0dce3e6d172770
Nelson Roberts <nellyroberts1968@gmail.com>
2022-03-24 00:20:00 -0700

    Backend for deployment

---
+++ server/server.js
@@ -0,0 +1,18 @@
+require("dotenv").config
+const express = require ("express");
+const cors =require("cors");
+const path = require("path");
+
+const app = express();
+//creating middleware
+app.use(express.json);
+app.use(cors())
+
+if(process.env.NODE_ENV === "production")
+app.use(express.static("client/build"))
+app.get("*",(req,res)=>(
+    res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+))
+
+const port = process.env.PORT || 3001;
+app.listen(port, console.log(`server listenrning to port 3001`))
