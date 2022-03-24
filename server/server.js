bf1f7f63e3cb6dab001f9d69a19db2c838eba8c7
Nelson Roberts <nellyroberts1968@gmail.com>
2022-03-24 11:41:17 -0700

    changed connection to fix deploye issue

--- server/server.js
+++ server/server.js
@@ -2,27 +2,26 @@ require("dotenv").config
 const express = require ("express");
 const cors =require("cors");
 const path = require("path");
-const timeout = require('connect-timeout');
+const db = require('./config/connection');
+require("dotenv").config()
 
 
-
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 //creating middleware
 app.use(express.json);
 app.use(cors())
-// app.use(timeout(10));
 
-if(process.env.NODE_ENV === "production")
-app.use(express.static("../client/build"))
-app.get("*",(req,res)=>(
-    res.sendFile(path.resolve(__dirname,"../client/build/index.html"))
-))
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, 'public')));
-
-
-
-app.listen(port, console.log(`server listenrning to port 3000`))
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+    });
+  });
